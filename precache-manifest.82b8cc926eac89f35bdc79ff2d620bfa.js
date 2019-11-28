8034850f0d056de20a80f0c464b0ea5e72a6a0b6
Nuno Macedo <nunomacedo@Nunos-MacBook-Air.local>
2019-11-28 10:51:45 -0500

    Updates

---
+++ precache-manifest.82b8cc926eac89f35bdc79ff2d620bfa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c551f1ca604181deab93a2538f658e21",
+    "url": "/gh-pages-app/index.html"
+  },
+  {
+    "revision": "dd5cca8d3ca99a5c5392",
+    "url": "/gh-pages-app/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "566977cd107981fa4003",
+    "url": "/gh-pages-app/static/js/2.f0e4f202.chunk.js"
+  },
+  {
+    "revision": "dd5cca8d3ca99a5c5392",
+    "url": "/gh-pages-app/static/js/main.c76e3489.chunk.js"
+  },
+  {
+    "revision": "e165b08d794ffde0ad3d",
+    "url": "/gh-pages-app/static/js/runtime-main.2c74c748.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/gh-pages-app/static/media/logo.25bf045c.svg"
+  }
+]);
